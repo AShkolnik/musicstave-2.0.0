67eb2049d2734c3ffbcc28175f9a35a3c69df5e5
Anton Shkolnik <anton.shkolnik@stud.hn.de>
2022-03-24 15:18:23 +0100

    Making Toolkit importable with first vector_field plugin

--- gamera/toolkits/musicstaves/__init__.py
+++ gamera/toolkits/musicstaves/__init__.py
@@ -8,13 +8,10 @@ and may be omitted if you are not concerned with that.
 
 from gamera import toolkit
 import wx
-from gamera.toolkits.skeleton import main
+#import plugins
+#from gamera.toolkits.musicstaves import main
 
-# Let's import all our plugins here so that when this toolkit
-# is imported using the "Toolkit" menu in the Gamera GUI
-# everything works.
-
-from gamera.toolkits.skeleton.plugins import clear
+#from gamera.toolkits.musicstaves.plugins import vector_field
 
 # You can inherit from toolkit.CustomMenu to create a menu
 # for your toolkit.  Create a list of menu option in the
@@ -22,13 +19,9 @@ from gamera.toolkits.skeleton.plugins import clear
 # correspond to them.  The name of the callback function
 # should be the same as the menu item, prefixed by '_On'
 # and with all spaces converted to underscores.
-class SkeletonMenu(toolkit.CustomMenu):
-    _items = ["Skeleton Toolkit",
-              "Skeleton Toolkit 2"]
-    def _OnSkeleton_Toolkit(self, event):
-        wx.MessageDialog(None, "You clicked on Skeleton Toolkit!").ShowModal()
+class MusicstaveMenu(toolkit.CustomMenu):
+    _items = ["Musicstave Toolkit"]
+    def _OnMusicstave_Toolkit(self, event):
+        wx.MessageDialog(None, "You clicked on Musicstave Toolkit!").ShowModal()
         main.main()
-    def _OnSkeleton_Toolkit_2(self, event):
-        wx.MessageDialog(None, "You clicked on Skeleton Toolkit 2!").ShowModal()
-        main.main()
-skeleton_menu = SkeletonMenu()
+musicstave_menu = MusicstaveMenu()
